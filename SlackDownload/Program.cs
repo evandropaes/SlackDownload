8dc102d9880af350f6f30f6fad130fe0106d0744
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2018-04-25 10:39:54 -0300

    Hints added
    This change adds some hints to achieve the required functionality, although the
    implementation details should remain to the final implementer.

--- SlackDownload/Program.cs
+++ SlackDownload/Program.cs
@@ -1,59 +1,78 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 namespace SlackDownload
 
 {
-    class Program
-    {
+    class Program {
 
-        string SourceFolder = @"C:\Windows\Temp";
+        private static string SourceFolder = @"C:\Windows\Temp";
 
-        static void Main(string[] args)
-        {
-            if (args.Length == 0 || args == null)
-            {
-                ShowError();
+        public static void Main(string[] args) {
+            // First check if any parameter is given, if none, show summary help
+            if (args.Length == 0 || args == null) {
+                ShowInfo();
             }
-            
-            string help = Console.ReadLine().ToLower();
-            if (help == "help")
-            {
-                ShowFullHelp();
+            else {
+                // If parameters were given, test them and collect its information
+                // ... Perform the test of for -h and then call the method ShowHelp()
+                // ... Other parameters could be added / tested later... ;-)
             }
-            Console.ReadKey();
+            // Execute the authentication (SlackAuthenticate)
+            // Indicate the slack channel to access (ChooseChannel)
+            // Collect the files to download (GetChannelFiles)
+            // Effectively download the files to local "temp" (DownloadArchives)
+            // Move files from local "temp" to final destination (CopyingArchives)
+            // ... Don't forget to check exceptions on external / critical sections...
+            // ... Hint:
+            //     - Check for common operations on the methods below to possible isolation in
+            //       standalone, generic and reusable methods
         }
 
-        public static void ShowError()
-        {
+        public static void ShowInfo() {
+            // Show information about the application, including the -h parameter for a full help
             Console.WriteLine("Error 404, Parameters Not Found");
             Console.WriteLine("Type 'help' for more information");
         }
 
-        public static void ShowFullHelp()
-        {
+        public static void ShowHelp() {
+            // This should print a full help, with all the supported parameters
             Console.WriteLine("\nParameters Accepted:\n\nFor user token, use xoxp-\n\nFor bot user token, use xoxb-\n\nFor workspace token, use xoxa- ");
         }
         
-        public static void SlackAutenticate()
-        {
-
+        public static async void SlackAuthenticate() {
+            // Prepare the client HTTP request
+            // Perform the call to the final URI entry point to validate your tokens
+            // Serialize the JSON returned
         }
 
-        public static void ChooseChannel()
-        {
-
+        public static async void ChooseChannel() {
+            // Prepare the client HTTP request
+            // Perform the call to the final URI entry point to get channel ID
+            // Serialize the JSON returned
         }
 
-        public static void DownloadArchives()
-        {
-            
+        public static async void GetChannelFiles() {
+            // Prepare the client HTTP request
+            // Perform the call to the final URI entry point to get available files
+            // Serialize the JSON returned
         }
 
-        public static void CopyingArchives()
-        {
-            
+        public static async void DownloadArchives() {
+            // Prepare the client HTTP request
+            // Perform the call to the final URI entry point to get the file
+            // Serialize the JSON returned
+            // Accept the file and save it to local "temp"
         }
-        
+
+        public static void CopyingArchives() {
+            // Move the downloaded files to the final destination
+        }
     }
 }
